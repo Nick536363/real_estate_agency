97f354a459e317082564971e953bd5db93fa9c28
Nick536363 <3383371nik@gmail.com>
2025-10-09 17:51:19 +0200

    добавил поиск в панель админа

--- property/admin.py
+++ property/admin.py
@@ -1,6 +1,9 @@
 from django.contrib import admin
-
 from .models import Flat
 
 
-admin.site.register(Flat)
+class Admin(admin.ModelAdmin):
+    search_fields = ("address", "town", "owner")
+
+
+admin.site.register(Flat, Admin)
